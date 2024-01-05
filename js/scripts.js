e0bbb46cef86710bc08be8592a4bb4242259692c
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-01-04 19:04:57 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+let pokemonList = [
+    {
+        name: 'Ivysaur', 
+        height: 1, 
+        type: ['grass', 'poison']
+    }
+    {
+        name: 'Ninetales', 
+        height: 1.1, 
+        type: ['fire']
+    }
+    {
+        name: 'Arcanine', 
+        height: 1.9, 
+        type: ['fire']
+    }
+    {
+        name: 'Charizard', 
+        height: 1.7, 
+        type: ['fire', 'flying']
+    }
+    {
+        name: 'Snorlax', 
+        height: 2.1, 
+        type: ['normal']
+    }
+    
+    
+]
