355097741963a897840183da71ee5d81bd84a98b
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-01-05 11:33:11 -0700

    Update scripts.js
    Added small fixes to help with future code and readability.

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,27 @@ let pokemonList = [
     {
         name: 'Ivysaur', 
         height: 1, 
-        type: ['grass', 'poison']
-    }
+        types: ['grass', 'poison']
+    },
     {
         name: 'Ninetales', 
         height: 1.1, 
         type: ['fire']
-    }
+    },
     {
         name: 'Arcanine', 
         height: 1.9, 
-        type: ['fire']
-    }
+        types: ['fire']
+    },
     {
         name: 'Charizard', 
         height: 1.7, 
-        type: ['fire', 'flying']
-    }
+        types: ['fire', 'flying']
+    },
     {
         name: 'Snorlax', 
         height: 2.1, 
-        type: ['normal']
-    }
+        types: ['normal']
+    },
     
     
-]
