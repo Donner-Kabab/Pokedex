4f2ac875fb10b7b379a25ec8551f1b6403d61e95
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-01-06 17:28:16 -0700

    Update scripts.js
    printed the names and height of the Pokemon and specified which one is the biggest

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
 let pokemonList = [
-    {
-        name: 'Ivysaur', 
-        height: 1, 
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Ninetales', 
-        height: 1.1, 
-        type: ['fire']
-    },
-    {
-        name: 'Arcanine', 
-        height: 1.9, 
-        types: ['fire']
-    },
-    {
-        name: 'Charizard', 
-        height: 1.7, 
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Snorlax', 
-        height: 2.1, 
-        types: ['normal']
-    },
-    
-    
+  {
+    name: "Ivysaur (height: 1)",
+    height: 1,
+    type: ["grass", "poison"],
+  },
+
+  {
+    name: "Ninetales (height: 1.1)",
+    height: 1.1,
+    type: ["fire"],
+  },
+
+  {
+    name: "Arcanine (height: 1.9)",
+    height: 1.9,
+    type: ["fire"],
+  },
+
+  {
+    name: "Charizard (height: 1.7)",
+    height: 1.7,
+    type: ["fire", "flying"],
+  },
+
+  {
+    name: "Snorlax (height: 2.1)",
+    height: 2.1,
+    type: ["normal"],
+  },
+];
+//There has to be a comma after the quotations above or it won't print
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name);
+  if (pokemonList[i].height > 2) {
+    document.write(pokemonList[i].name + " - Wow, that's big!");
+  }
+}
